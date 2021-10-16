2e4f0eaba09619613011f2f03d4a9e9cfae218d7
purvap <92616748+purva0931@users.noreply.github.com>
2021-10-16 20:53:12 +0530

    Create Footer.js

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import Style from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={Style.foot}>
+      <p>Copyright &copy; 2021 - Purva Padmawar</p>
+    </footer>
+  );
+};
+
+export default Footer;
