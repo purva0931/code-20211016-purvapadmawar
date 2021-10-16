d7b408efd600b12b28878e9f60417fbc41a97abd
purvap <92616748+purva0931@users.noreply.github.com>
2021-10-16 20:57:37 +0530

    Create CarDetails.js

---
+++ src/Data/CarDetails.js
@@ -0,0 +1,112 @@
+export default [
+  {
+    id: 111,
+    title: "MG Astor",
+    description:
+      "The Astor is a handsome crossover that has a bunch of new-age features and equipment, and it isn’t half as bad when it comes to driving dynamics too. It sure looks like MG has a winner on their hands.",
+    artist: "MG",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/51940/astor-exterior-right-front-three-quarter.jpeg?isig=0&q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/370x208/n/cw/ec/51940/astor-exterior-right-front-three-quarter.jpeg?isig=0&q=85",
+    accentColor: "#f52459",
+    backgroundColor: "#841225",
+    textColor: "#ffffff",
+    price: "9.78 Lakh",
+  },
+  {
+    id: 114,
+    title: "Mahindra XUV700",
+    description:
+      "Looking at the prices, Mahindra is clearly elevating the value co-efficient with the new XUV700. Be it features, space, comfort, safety, or even performance, the XUV manages to deliver on all fronts with aplomb.",
+    artist: "Mahindra",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/42355/xuv700-exterior-right-front-three-quarter.jpeg?isig=0&q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/42355/xuv700-exterior-right-front-three-quarter.jpeg?isig=0&q=85",
+    accentColor: "#8a8989",
+    backgroundColor: "#424242",
+    textColor: "#FFCCCC",
+    price: "12.49 Lakhs",
+  },
+  {
+    id: 115,
+    title: "Volkswagen Taigun",
+    description:
+      "This all-new compact SUV embodies Volkswagen in every possible manner, right from the way it drives, to the way it feels and appears.",
+    artist: "Volkswagen",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/45057/taigun-exterior-right-front-three-quarter-2.jpeg?q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/45057/taigun-exterior-right-front-three-quarter-2.jpeg?q=85",
+    accentColor: "#00b6b8",
+    backgroundColor: "#666666",
+    textColor: "#08070b",
+    price: "10.49 Lakhs",
+  },
+  {
+    id: 118,
+    title: "Hyundai i20 N Line",
+    description:
+      "After being successful in the world of rallying over the past few years, the i20 N Line now makes an entry in our country. With a racing hot-hatchback stance and performance to match, it is simply impossible to not get tempted.",
+    artist: "Hyundai",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/100121/i20-n-line-exterior-right-front-three-quarter-12.jpeg?isig=0&q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/100121/i20-n-line-exterior-right-front-three-quarter-12.jpeg?isig=0&q=85",
+    accentColor: "#e31718",
+    backgroundColor: "#1f0809",
+    textColor: "#ffffff",
+    price: "9.84 Lakh",
+  },
+  {
+    id: 121,
+    title: "Volvo XC60 Facelift",
+    description:
+      "Volvo Cars India unveiled the XC60 facelift back in March 2021. The Swedish automobile marque is now set to introduce the updated SUV in the country soon.",
+    artist: "Volvo",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/104723/exterior-right-front-three-quarter.jpeg?isig=0&q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/104723/exterior-right-front-three-quarter.jpeg?isig=0&q=85",
+    accentColor: "#a3ad70",
+    backgroundColor: "#0d0a10",
+    textColor: "#ffffff",
+  },
+  {
+    id: 112,
+    title: "Audi Q5 Facelift",
+    description:
+      "The Audi Q5 facelift was unveiled in June last year, and now, the company has begun testing the model on Indian soil ahead of its launch that is likely to take place later this year. Upon launch, the model will rival the Mercedes-Benz GLC Class, BMW X3, and the Land Rover Discovery Sport.",
+    artist: "Audi",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/53591/q5-facelift-exterior-right-front-three-quarter.jpeg?q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/53591/q5-facelift-exterior-right-front-three-quarter.jpeg?q=85",
+    accentColor: "#377b35",
+    backgroundColor: "#261e1c",
+    textColor: "#ffffff",
+    price: "53.00 - 58.00 Lakhs",
+  },
+  {
+    id: 125,
+    title: "Tata Punch",
+    description:
+      "When it arrives, the Punch will enter into the newly formed micro SUV segment and will rival against the likes of the Ford Freestyle, Mahindra KUV100 and the Maruti Suzuki Ignis.",
+    artist: "Tata Motors",
+    isFree: false,
+    image1:
+      "https://imgd.aeplcdn.com/1056x594/n/cw/ec/39015/punch-exterior-right-front-three-quarter-2.jpeg?isig=0&q=85&wm=1",
+    image2:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/39015/punch-exterior-right-front-three-quarter-2.jpeg?isig=0&q=85",
+    accentColor: "#ef3f3a",
+    backgroundColor: "#131a1d",
+    textColor: "#ffffff",
+  },
+];
